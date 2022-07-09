6b869e40537047c5ce862d78c66c538283092fea
Scott Atkinson <scott.atkin79@gmail.com>
2022-07-09 15:05:01 +0000

    re arrange files and folders to products app

--- products/models.py
+++ products/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 
 class Category(models.Model):
-    name  = models.CharField(max_length=254)
-    friendly_name  = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
     def __str__(self):
         return self.name
@@ -14,13 +14,13 @@ class Category(models.Model):
 
 class Product(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
-    sku  = models.CharField(max_length=254, null=True, blank=True)
-    name  = models.CharField(max_length=254)
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=254)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
-    images_url = models.URLField(max_length=1204, null=True, blank=True)
-    images = models.URLField(null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return self.name
